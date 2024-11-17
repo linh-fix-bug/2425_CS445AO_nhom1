69dc5dc919390a707266d4c1a724ef318816c6a1
linh-fix-bug <nguyenphuoclinh26@gmail.com>
2024-11-17 13:44:23 +0700

    Add project files.

---
+++ DO_AN_CHUYEN_NGHANH/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DO_AN_CHUYEN_NGHANH.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HienthiTen()
+        {
+            return View();
+        }
+    }
+}
---
+++ DO_AN_CHUYEN_NGHANH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DO_AN_CHUYEN_NGHANH.MvcApplication" Language="C#" %>
