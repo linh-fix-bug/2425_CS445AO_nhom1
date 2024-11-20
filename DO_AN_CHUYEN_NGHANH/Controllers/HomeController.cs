5c30fde0dee0673f5b6a8625ec23bbbf1366692f
linh-fix-bug <nguyenphuoclinh26@gmail.com>
2024-11-20 23:40:01 +0700

    test dữ liệu

--- DO_AN_CHUYEN_NGHANH/Controllers/HomeController.cs
+++ DO_AN_CHUYEN_NGHANH/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DO_AN_CHUYEN_NGHANH.Models;
 
 namespace DO_AN_CHUYEN_NGHANH.Controllers
 {
@@ -11,7 +12,9 @@ namespace DO_AN_CHUYEN_NGHANH.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            QUANLYTROEntities db = new QUANLYTROEntities();
+            var danhsachPhong = db.Phongs.ToList();
+            return View(danhsachPhong);
         }
        
     }
