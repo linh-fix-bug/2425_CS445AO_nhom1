43397a9ccbc5722d502cefacbcbea04a47de673f
Admin <Admin@LAPTOP-M5MQ9R51>
2024-12-16 22:14:47 +0700

    Nguyen Tan Dung

--- DO_AN_CHUYEN_NGHANH/Controllers/DungChung/DangKiController.cs
+++ DO_AN_CHUYEN_NGHANH/Controllers/DungChung/DangKiController.cs
@@ -1,1 +1,62 @@
-﻿
+﻿using DO_AN_CHUYEN_NGHANH.Controllers;
+using DO_AN_CHUYEN_NGHANH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DO_AN_CHUYEN_NGHANH.Controllers
+{
+    public class DangKiController : Controller
+    {
+        // GET: DangKi
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string tenDangKi, string matKhau, string nhapLaiMatKhau)
+        {
+            // Kiểm tra đầu vào
+            if (string.IsNullOrEmpty(tenDangKi))
+            {
+                ViewBag.thongbao = "Không được để rỗng";
+                return View();
+            }
+            if (!tenDangKi.EndsWith("@gmail.com"))
+            {
+                ViewBag.thongbao = "Vui lòng đăng ký bằng định dạng @gmail.com!";
+                return View();
+            }
+            if (string.IsNullOrEmpty(matKhau) || matKhau.Length < 6)
+            {
+                ViewBag.thongbao = "Mật khẩu phải có ít nhất 6 ký tự.";
+
+                return View();
+            }
+            if (matKhau != nhapLaiMatKhau)
+            {
+                ViewBag.thongbao = "Mật khẩu nhập lại không trùng khớp.";
+
+                return View();
+            }
+
+            // Gọi hàm đăng ký
+            var result = new mapTaiKhoan().DangKi(tenDangKi, matKhau);
+            if (!result) // Nếu đăng ký thất bại
+            {
+                ViewBag.thongbao = "Đăng ký thất bại. Email có thể đã tồn tại hoặc có lỗi hệ thống.";
+                return View();
+            }
+
+            // Đăng ký thành công
+            TempData["SuccessMessage"] = "Đăng ký thành công. Vui lòng đăng nhập.";
+            return RedirectToAction("Index", "Login");
+        }
+
+
+    }
+}
--- DO_AN_CHUYEN_NGHANH/Controllers/DungChung/LoginController.cs
+++ DO_AN_CHUYEN_NGHANH/Controllers/DungChung/LoginController.cs
@@ -1,1 +1,68 @@
-﻿
+﻿using DO_AN_CHUYEN_NGHANH.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DO_AN_CHUYEN_NGHANH.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index(string tenDangNhap, string matKhau)
+        {
+
+            if (string.IsNullOrEmpty(tenDangNhap) && string.IsNullOrEmpty(matKhau))
+            {
+                return View();
+            }
+            if (string.IsNullOrEmpty(tenDangNhap) || string.IsNullOrEmpty(matKhau))
+            {
+                ViewBag.thongbao = "Nhập đầy đủ thông tin";
+                return View();
+            }
+
+            var taikhoan = new mapTaiKhoan().chitiet(tenDangNhap);
+
+
+            if (taikhoan == null)
+            {
+                ViewBag.thongbao = "Tài khoản hoặc mật khẩu không đúng";
+                ViewBag.tenDangNhap = tenDangNhap;
+                return View();
+            }
+
+
+            if (matKhau != taikhoan.MatKhau)
+            {
+                ViewBag.thongbao = "Tài khoản hoặc mật khẩu không đúng";
+                ViewBag.tenDangNhap = tenDangNhap;
+                return View();
+            }
+
+            Session["user"] = taikhoan;
+
+            if (tenDangNhap.Equals("admin", StringComparison.OrdinalIgnoreCase))
+            {
+                return Redirect("/HomeAdmin");
+            }
+            else
+            {
+                return Redirect("/HomeKhach");
+            }
+        }
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            Session["user"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+
+
+
+
+    }
+}
